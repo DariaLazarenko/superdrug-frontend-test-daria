9aa3754364a310b05b1898754b064652cc29ef8a
Daria Lazarenko <lazarenko200454@gmail.com>
2024-10-26 15:48:50 +0100

    Add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CatalogueComponent } from './components/catalogue/catalogue.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CatalogueComponent],
+  imports: [RouterOutlet, CatalogueComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'superdrug-frontend-test-daria';
-}
+export class AppComponent {}
